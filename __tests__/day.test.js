343c2b7509cae166dc659fab6c41e99cc012122b
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-24 10:55:41 -0700

    pass test to add one activity block and subtract one from day available
    co-authored-by: Tom McDowell <tmcdowellddhs@yahoo.com>
    dquote> co-authored-by: Emma Gerigscott <elgerig@gmail.com>
    dquote> co-authored-by: Kyle Crawford <kcraw217@gmail.com>

--- __tests__/day.test.js
+++ __tests__/day.test.js
@@ -1,10 +1,25 @@
+import Activity from "../src/activity";
 import Day from "../src/day";
 
 describe('Day', () => {
+  let monday;
+  let activity;
+  let available;
+
+  beforeEach(() => {
+    available = 5;
+    monday = new Day(available);
+    activity = new Activity("salad");
+    monday.addActivity(activity);
+  })
 
   test('it should add an activity with a unique id to the Day object', () => {
-    let monday = new Day(6);
-    monday.addActivity("salad");
-    expect(monday.sections).toEqual({ 1: "salad" });
+    expect(monday.activities).toEqual({ "salad": { name: "salad", blocks: 0 } });
   });
+
+  test('it should add one to activity block and subtract one from day availble', () => {
+    monday.addActivityBlocks("salad");
+    expect(monday.available).toEqual(19);
+    expect(monday.activities["salad"].blocks).toEqual(1);
+  })
 });

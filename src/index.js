ae8edf7acedb9013c3f552f35c9101d6a5c5aadf
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-24 11:53:19 -0700

    add user input form and display result
    co-authored-by: Tom McDowell <tmcdowellddhs@yahoo.com>
    dquote> co-authored-by: Emma Gerigscott <elgerig@gmail.com>
    dquote> co-authored-by: Kyle Crawford <kcraw217@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Day from './day.js';
+import Activity from './activity.js';
+
+// Business Logic
+
+
+
+
+// UI Logic
+
+function displayDay(day) {
+  document.getElementById("available").innerText = day.available;
+  let keys = Object.keys(day.activities);
+  document.getElementById("activity").innerText = day.activities[keys[0]].name;
+
+}
+
+function handleFormSubmission(e) {
+  e.preventDefault();
+  const userFreeTime = document.getElementById("free-time").value;
+  const userActivity = document.getElementById("activity1").value;
+  let today = new Day(userFreeTime);
+  let activity = new Activity(userActivity);
+  today.addActivity(activity);
+  displayDay(today);
+}
+
+document.querySelector("form").addEventListener("submit", handleFormSubmission);

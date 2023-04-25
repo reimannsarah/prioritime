f9f2b33e16612108fca24d762b085b7bac395727
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-25 10:16:22 -0700

    refactor test to include all days of the week in class constructor, pass test

--- __tests__/week.test.js
+++ __tests__/week.test.js
@@ -1,8 +1,8 @@
 import { Week } from '../src/week.js';
 
 describe('Week', () => {
-  test('it should create a new instance of week one key of Monday', () => {
+  test('it should create a new instance of week with keys that correspond to the days of the week', () => {
     let newWeek = new Week();
-    expect(newWeek).toEqual({"monday":{}});
+    expect(newWeek).toEqual({"monday":{},"tuesday":{},"wednesday":{},"thursday":{},"friday":{},"saturday":{},"sunday":{}});
   })
 });
--- src/week.js
+++ src/week.js
@@ -1,6 +1,12 @@
 export class Week {
   constructor() {
     this.monday = {};
+    this.tuesday = {};
+    this.wednesday = {};
+    this.thursday = {};
+    this.friday = {};
+    this.saturday = {};
+    this.sunday = {};
   }
 };
 
